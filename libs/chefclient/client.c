fb7d76695610a8ddb3a10ab21cc08d553265c71c
Philip Meulengracht <the_meulengracht@hotmail.com>
2022-02-26 14:29:41 +0100

    libs/chefclient: can now retrieve a device code

--- libs/chefclient/client.c
+++ libs/chefclient/client.c
@@ -17,6 +17,7 @@
  */
 
 #include <curl/curl.h>
+#include <wolfssl/ssl.h>
 #include <errno.h>
 #include <libchefclient.h>
 #include <stdlib.h>
@@ -53,9 +54,104 @@ static int __get_device_auth_body(char* buffer, size_t maxLength)
     int written = snprintf(buffer, maxLength,
         "client_id=%s&scope=%s",
         g_chefClientId,
-        "user.read%%20openid%%20profile"
+        "user.read%20openid%20profile"
     );
     return written < maxLength ? 0 : -1;
+}
+
+struct data {
+  char trace_ascii; /* 1 or 0 */
+};
+ 
+static
+void dump(const char *text,
+          FILE *stream, unsigned char *ptr, size_t size,
+          char nohex)
+{
+  size_t i;
+  size_t c;
+ 
+  unsigned int width = 0x10;
+ 
+  if(nohex)
+    /* without the hex output, we can fit more on screen */
+    width = 0x40;
+ 
+  fprintf(stream, "%s, %10.10lu bytes (0x%8.8lx)\n",
+          text, (unsigned long)size, (unsigned long)size);
+ 
+  for(i = 0; i<size; i += width) {
+ 
+    fprintf(stream, "%4.4lx: ", (unsigned long)i);
+ 
+    if(!nohex) {
+      /* hex not disabled, show it */
+      for(c = 0; c < width; c++)
+        if(i + c < size)
+          fprintf(stream, "%02x ", ptr[i + c]);
+        else
+          fputs("   ", stream);
+    }
+ 
+    for(c = 0; (c < width) && (i + c < size); c++) {
+      /* check for 0D0A; if found, skip past and start a new line of output */
+      if(nohex && (i + c + 1 < size) && ptr[i + c] == 0x0D &&
+         ptr[i + c + 1] == 0x0A) {
+        i += (c + 2 - width);
+        break;
+      }
+      fprintf(stream, "%c",
+              (ptr[i + c] >= 0x20) && (ptr[i + c]<0x80)?ptr[i + c]:'.');
+      /* check again for 0D0A, to avoid an extra \n if it's at width */
+      if(nohex && (i + c + 2 < size) && ptr[i + c + 1] == 0x0D &&
+         ptr[i + c + 2] == 0x0A) {
+        i += (c + 3 - width);
+        break;
+      }
+    }
+    fputc('\n', stream); /* newline */
+  }
+  fflush(stream);
+}
+ 
+static
+int my_trace(CURL *handle, curl_infotype type,
+             char *data, size_t size,
+             void *userp)
+{
+  struct data *config = (struct data *)userp;
+  const char *text;
+  (void)handle; /* prevent compiler warning */
+ 
+  switch(type) {
+  case CURLINFO_TEXT:
+    fprintf(stderr, "== Info: %s", data);
+    /* FALLTHROUGH */
+  default: /* in case a new one is introduced to shock us */
+    return 0;
+ 
+  case CURLINFO_HEADER_OUT:
+    text = "=> Send header";
+    break;
+  case CURLINFO_DATA_OUT:
+    text = "=> Send data";
+    break;
+  case CURLINFO_SSL_DATA_OUT:
+    text = "=> Send SSL data";
+    break;
+  case CURLINFO_HEADER_IN:
+    text = "<= Recv header";
+    break;
+  case CURLINFO_DATA_IN:
+    text = "<= Recv data";
+    break;
+  case CURLINFO_SSL_DATA_IN:
+    text = "<= Recv SSL data";
+    break;
+  }
+ 
+  dump(text, stderr, (unsigned char *)data, size, config->trace_ascii);
+  return 0;
 }
 
 static int __oauth2_device_flow_start(void)
@@ -64,6 +160,10 @@ static int __oauth2_device_flow_start(void)
     CURLcode code;
     size_t   dataIndex = 0;
     char     buffer[256];
+    struct data config;
+    
+    config.trace_ascii = 1; /* enable ascii tracing */
+
 
     // initialize a curl session
     curl = curl_easy_init();
@@ -77,6 +177,15 @@ static int __oauth2_device_flow_start(void)
         fprintf(stderr, "__oauth2_device_flow_start: failed to set error buffer [%d]\n", code);
         return -1;
     }
+
+    curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, my_trace);
+    curl_easy_setopt(curl, CURLOPT_DEBUGDATA, &config);
+ 
+    /* the DEBUGFUNCTION has no effect until we enable VERBOSE */
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+
+    // To get around CA cert issues
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
  
     // set the url
     __get_devicecode_auth_link(buffer, sizeof(buffer));
