6f5cfa8254d8277aa0e3ec827cb767227e5d34bd
Philip Meulengracht <the_meulengracht@hotmail.com>
2022-03-24 15:12:41 +0100

    tools/serve: add skeleton

---
+++ tools/serve/commands/update.c
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2022, Philip Meulengracht
+ *
+ * This program is free software : you can redistribute it and / or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation ? , either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+static void __print_help(void)
+{
+    printf("Usage: serve update [options]\n");
+    printf("Options:\n");
+    printf("  -h, --help\n");
+    printf("      Print this help message\n");
+}
+
+int update_main(int argc, char** argv)
+{
+    int status = 0;
+
+    if (argc > 2) {
+        for (int i = 2; i < argc; i++) {
+            if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
+                __print_help();
+                return 0;
+            }
+        }
+    }
+
+    return status;
+}
