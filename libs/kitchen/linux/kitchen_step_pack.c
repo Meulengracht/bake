b12f73a15e8873bc2fbf6f7770d84638deac7e8e
Philip Meulengracht <the_meulengracht@hotmail.com>
2024-05-23 13:28:37 +0200

    libs/kitchen: swap a couple of names in resulting pack

--- libs/kitchen/linux/kitchen_step_pack.c
+++ libs/kitchen/linux/kitchen_step_pack.c
@@ -65,7 +65,7 @@ static char* __source_pack_name(const char* root, const char* name)
 static char* __destination_pack_name(const char* root, const char* platform, const char* arch, const char* name)
 {
     char tmp[4096] = { 0 };
-    snprintf(&tmp[0], sizeof(tmp), "%s/%s_%s_%s.pack", root, platform, arch, name);
+    snprintf(&tmp[0], sizeof(tmp), "%s/%s_%s_%s.pack", root, name, platform, arch);
     return strdup(&tmp[0]);
 }
 
