c9130967eb4dd14cfa345cc810fc9d6862ca4a8a
Oliver Meulengracht <Olivermeulengracht@protonmail.com>
2024-06-13 12:06:17 +0200

    libs/platform/windows: implemented abspath and unlink (#10)
    * libs/platform/windows: implemented abspath and unlink
    
    * feedback changes: implemented suggestion
    
    * Review changes

---
+++ libs/platform/osutils/windows/abspath.c
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2024, Philip Meulengracht
+ *
+ * This program is free software : you can redistribute it and / or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation ? , either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#include <chef/platform.h>
+#include <stdlib.h>
+
+char* platform_abspath(const char* path)
+{
+    char *fullPath = calloc(1, MAX_PATH);
+    if (fullPath == NULL) {
+        return NULL;
+    }
+
+    if (GetFullPathName(path, MAX_PATH, fullPath, NULL) == 0) {
+        free(fullPath);
+        return NULL;
+    }
+    
+    return fullPath;
+}
