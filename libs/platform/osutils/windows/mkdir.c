f64b9da4f8585242a3286ebd8ca78448c81a4b35
Oliver Meulengracht <Olivermeulengracht@protonmail.com>
2024-06-26 21:26:30 +0200

    libs/platform/windows: cleanup

--- libs/platform/osutils/windows/mkdir.c
+++ libs/platform/osutils/windows/mkdir.c
@@ -22,64 +22,14 @@
 #include <string.h>
 #include <sys/stat.h>
 
-int __directory_exists(const wchar_t* path) 
-{
-    DWORD attribs = GetFileAttributesW(path);
-    if (attribs == INVALID_FILE_ATTRIBUTES) {
-        return 0;
-    }
-    return (attribs & FILE_ATTRIBUTE_DIRECTORY) ? 1 : -1;
-}
-
 wchar_t* mb_to_wcs(const char* path) 
 {
-    size_t length = strlen(path) + 1;
-    wchar_t* wpath = (wchar_t*)malloc((length + 1) * sizeof(wchar_t));
-    if (wpath) {
-        mbstowcs(wpath, path, length);
+    size_t pathLength = strlen(path);
+    wchar_t* wpath = (wchar_t*)calloc(sizeof(wchar_t), pathLength + 10);
+    if (wpath != NULL) {
+        mbstowcs(wpath, path, pathLength);
     }
     return wpath;
-}
-
-static int __mkdir(const char* path) 
-{
-    wchar_t* wpath = mb_to_wcs(path);
-    if (!wpath) {
-        return -1;
-    }
-
-    int status = __directory_exists(wpath);
-    if (status == 1) {
-        free(wpath);
-        return 0;
-    }
-
-    char* sub_path = _strdup(path);
-    if (!sub_path) {
-        free(wpath);
-        return -1;
-    }
-
-    char* last_separator = strrchr(sub_path, '\\');
-    if (last_separator != NULL) {
-        *last_separator = '\0';
-        if (__mkdir(sub_path) != 0) {
-            free(sub_path);
-            free(wpath);
-            return -1;
-        }
-    }
-    free(sub_path);
-
-    if (CreateDirectoryW(wpath, NULL) || GetLastError() == ERROR_ALREADY_EXISTS) {
-        free(wpath);
-        return 0;
-    } else {
-        DWORD error = GetLastError();
-        printf("Error creating directory: %lu\n", error);
-        free(wpath);
-        return -1;
-    }
 }
 
 int platform_mkdir(const char* path) 
@@ -96,7 +46,7 @@ int platform_mkdir(const char* path)
     }
 
     wchar_t* p;
-    for (p = wpath + 1; *p; p++) {
+    for (wchar_t* p = wpath + 1; *p; p++) {
         if (*p == L'\\' || *p == L'/') {
             *p = L'\0';
             if (_wmkdir(wpath) != 0 && errno != EEXIST) {
