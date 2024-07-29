dc22507ad1fc143e73eb45da402daa57811856c8
Philip Meulengracht <the_meulengracht@hotmail.com>
2024-07-29 16:45:23 +0200

    many: some restructuring

---
+++ tools/cvrun/main.c
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2022, Philip Meulengracht
+ *
+ * This program is free software : you can redistribute it and / or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation ? , either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#include <containerv.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "chef-config.h"
+
+static void __print_help(void)
+{
+    printf("Usage: order <command> [options]\n");
+    printf("\n");
+    printf("Options:\n");
+    printf("  -h, --help\n");
+    printf("      Print this help message\n");
+    printf("  -v, --version\n");
+    printf("      Print the version of order\n");
+}
+
+int main(int argc, char** argv, char** envp)
+{
+    int result;
+
+    // first argument must be the command if not --help or --version
+    if (argc > 1) {
+        if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
+            __print_help();
+            return 0;
+        }
+
+        if (!strcmp(argv[1], "-v") || !strcmp(argv[1], "--version")) {
+            printf("order: version " PROJECT_VER "\n");
+            return 0;
+        }
+    }
+
+
+
+    return 0;
+}
