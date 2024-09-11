079d0bcc128f75d92e226e4383d8c8580bc4bfd4
Philip Meulengracht <the_meulengracht@hotmail.com>
2024-09-11 20:27:37 +0200

    daemons/waiterd: time for logging and conf

--- daemons/waiterd/server_main.c
+++ daemons/waiterd/server_main.c
@@ -18,6 +18,7 @@
 
 #include "private.h"
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 static char g_templateGuid[] = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
@@ -27,19 +28,86 @@ static struct waiterd_server g_server = { 0 };
 
 static struct waiterd_cook* __waiterd_cook_new(gracht_conn_t client)
 {
+    struct waiterd_cook* cook;
 
+    cook = calloc(1, sizeof(struct waiterd_cook));
+    if (cook == NULL) {
+        return NULL;
+    }
+
+    cook->client = client;
+    return cook;
 }
+
+static void __waiterd_cook_delete(struct waiterd_cook* cook) 
+{
+    // nothing to do here for now
+    free(cook);
+}
+
+struct waiterd_cook* __find_cook_by_client(gracht_conn_t client)
+{
+    struct list_item* i;
+
+    list_foreach(&g_server.cooks, i) {
+        struct waiterd_cook* cook = (struct waiterd_cook*)i;
+        if (cook->client == client) {
+            return cook;
+        }
+    }
+
+    return NULL;
+}
+
 
 void waiterd_server_cook_connect(gracht_conn_t client)
 {
-    // do nothing yet
+    struct waiterd_cook* cook = __waiterd_cook_new(client);
+    if (cook == NULL) {
+        // needs to be logged fatally
+        return;
+    }
+
+    list_add(&g_server.cooks, &cook->list_header);
 }
 
 void waiterd_server_cook_disconnect(gracht_conn_t client)
 {
+    struct waiterd_cook* cook = __find_cook_by_client(client);
+    struct list_item*    i;
+
+    // invalid cook?
+    if (cook == NULL) {
+        // log this
+        return;
+    }
+    
+    // remove cook immediately
+    list_remove(&g_server.cooks, &cook->list_header);
+
     // abort any request in flight for waiters
+    list_foreach(&g_server.requests, i) {
+        struct waiterd_request* request = (struct waiterd_request*)i;
+        if (request->cook == client) {
+            // abort
+        }
+    }
 
     // cleanup cook
+    __waiterd_cook_delete(cook);
+}
+
+void waiterd_server_cook_ready(gracht_conn_t client, enum waiterd_architecture arch)
+{
+    struct waiterd_cook* cook = __find_cook_by_client(client);
+
+    if (cook == NULL) {
+        // invalid cook, log this 
+        return;
+    }
+
+    cook->architecture = arch;
+    cook->ready = 1;
 }
 
 struct waiterd_cook* waiterd_server_cook_find(enum waiterd_architecture arch)
@@ -87,6 +155,7 @@ struct waiterd_request* waiterd_server_request_new(
         return NULL;
     }
     
+    request->cook = cook->client;
     request->source = malloc(GRACHT_MESSAGE_DEFERRABLE_SIZE(message));
     if (request->source == NULL) {
         free(request);
@@ -99,5 +168,14 @@ struct waiterd_request* waiterd_server_request_new(
 
 struct waiterd_request* waiterd_server_request_find(const char* id)
 {
+    struct list_item* i;
     
+    list_foreach(&g_server.requests, i) {
+        struct waiterd_request* request = (struct waiterd_request*)i;
+        if (!strcmp(request->guid, id)) {
+            return request;
+        }
+    }
+    
+    return NULL;
 }
