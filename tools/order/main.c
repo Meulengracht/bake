e2c7ee0c4a025eb4d6b6393ceff9c4f115e260fc
Philip Meulengracht <the_meulengracht@hotmail.com>
2026-04-17 00:33:56 +0200

    tools/order: break when a valid command is found

--- tools/order/main.c
+++ tools/order/main.c
@@ -97,6 +97,7 @@ int main(int argc, char** argv, char** envp)
                 fprintf(stderr, "order: invalid command %s\n", argv[i]);
                 return -1;
             }
+            break;
         }
     }
 
